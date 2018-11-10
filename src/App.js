6604aa5ac9817c4d4c0d5684ac64851d0ba96e43
pscroce <pscroce@gmail.com>
2018-11-10 14:38:43 -0500

    feat: conditional rendering of lineage select based on ancestry depth select

--- src/App.js
+++ src/App.js
@@ -1,15 +1,145 @@
 import React, { Component } from 'react';
+import Guide from './components/Guide';
+import Header from './components/Header';
+import Select from 'react-select';
 import './App.css';
+const ancestorOptions = [
+  { value: 'parent', label: 'Parent' },
+  { value: 'gParent', label: 'Grandparent' },
+  { value: 'ggParent', label: 'Great-grandparent' },
+
+];
+const options = [
+  { value: 'mother', label: 'Mother' },
+  { value: 'father', label: 'Father' },
+  { value: 'gmother', label: 'Grandmother' },
+  { value: 'gfather', label: 'Grandfather' },
+  { value: 'ggmother', label: 'Great-grandmother' },
+  { value: 'ggfather', label: 'Great-grandfather' }
+];
 
 class App extends Component {
+    state = {
+      ancestor: '',
+      parent: '',
+      gParent: '',
+      ggParent: ''
+    }
+  handleAncestorChange = (ancestor) => {
+    this.setState({ ancestor });
+  }
+  handleChange = (parent) => {
+    this.setState({ parent });
+  }
+  handleGParentChange = (gParent) => {
+    this.setState({ gParent });
+  }
+  handleGGParentChange = (ggParent) => {
+    this.setState({ ggParent });
+  }
   render() {
+    const { ancestor } = this.state;
+    const { parent } = this.state;
+    const { gParent } = this.state;
+    const { ggParent } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Italian Citizenship Guide</h1>
-        </header>
+        <Header></Header>
         <body>
-          
+          <Guide></Guide>
+
+          <div className="form-wrapper"
+            // style={styles.formWrapper}
+            >
+            <form
+              // onSubmit={this.handleSubmit} style={styles.form}
+              >
+
+                <div>
+                  if gg, show "select your gg's daughter or son (your g-mother or g-father)"
+                  if g, show "select your g's daughter or son (your mother or father)"
+                </div>
+                <h3>Select your Italian ancestor</h3>
+                <Select
+                  value={ancestor}
+                  onChange={this.handleAncestorChange}
+                  options={ancestorOptions}
+                />
+
+                {
+                  this.state.ancestor.value === 'parent' &&
+                  <div>
+                    <h3>Select your lineage</h3>
+                    <Select
+                      value={parent}
+                      onChange={this.handleChange}
+                      options={options}
+                    />
+                  </div>
+                }
+                {
+                  this.state.ancestor.value === 'gParent' &&
+                  <div>
+                    <h3>Select your lineage</h3>
+                    <Select
+                      value={parent}
+                      onChange={this.handleChange}
+                      options={options}
+                    />
+                    <Select
+                      value={gParent}
+                      onChange={this.handleGParentChange}
+                      options={options}
+                    />
+                  </div>
+                }
+                {
+                  this.state.ancestor.value === 'ggParent' &&
+                  <div>
+                    <h3>Select your lineage</h3>
+                    <Select
+                      value={parent}
+                      onChange={this.handleChange}
+                      options={options}
+                    />
+                    <Select
+                      value={gParent}
+                      onChange={this.handleGParentChange}
+                      options={options}
+                    />
+                    <Select
+                      value={ggParent}
+                      onChange={this.handleGGParentChange}
+                      options={options}
+                    />
+                  </div>
+                }
+
+              <div className="inputGroup">
+                <textarea type="text" rows="4" className="input-and-button input"
+                  // style={[styles.input, styles.inputAndButton]}
+                  // value={this.state.note}
+                  // onChange={(event) => this.setState({ note: event.target.value })}
+                  required >
+                </textarea>
+                <label htmlFor="Note"
+                  // style={styles.label}
+                  >
+                  Note
+                </label>
+              </div>
+
+              <button type="submit"
+                id="buttonSubmitForm" className="input-and-button button"
+                // style={this.state.submitted === false ? [styles.button, styles.inputAndButton] : [styles.button, styles.inputAndButton]}
+                >
+                Check Eligibility
+              </button>
+            </form>
+          </div>
+
+
         </body>
         <footer>Italian Citizenship Guide</footer>
       </div>
---
+++ src/components/Guide.js
@@ -0,0 +1,54 @@
+import React from 'react';
+
+function Guide() {
+  return (
+    <div className="guide__wrapper">
+      <h1>How to get Italian citizenship by ancestry</h1>
+        <h3>Identify your lineage</h3>
+        <ol>
+          <li>Your parent, grandparent, or great-grandparent must have been an Italian citizen</li>
+          <li>It must be an “unbroken line” from your Italian ancestor to you</li>
+          <li>No one along the line to you may have renounced their Italian citizenship before the birth of the connecting ancestor in your line</li>
+          <li>Births of your connecting ancestors in the line must have been in wedlock</li>
+          <li>Women may not pass citizenship to children born before 1948*</li>
+        </ol>
+      <p>
+      </p>
+      <ul>
+        <li>Exclusion applies to “children of an Italian citizen mother and an alien father born before 1 January 1948”</li>
+        <li>“If the child had an Italian mother and a foreign father, the child ordinarily must have been born on or after 1 January 1948. There have been many successful challenges to this date restriction that were brought before the Court of Rome. In their capacity to determine Italian citizenship, officers in Italian consulates and municipalities are bound by the restriction.” Wikipedia</li>
+      </ul>
+      <h3>Find your consulate</h3>
+      <ol>
+        <li>Book an appointment (first available is typically 1.5 - 2 yrs from date of booking)</li>
+      </ol>
+      <h3>Find your documents</h3>
+      <ol>
+        <li>For every ancestor, acquire all the following vital records:</li>
+        <ol>
+          <li>Birth certificate</li>
+          <li>Marriage certificate</li>
+          <li>Death certificate (if applicable)</li>
+          <li>Divorce judgment (if applicable)</li>
+        </ol>
+        <li>For every vital record, acquire an apostille from the issuing US state or country.</li>
+        <li>For the primary ancestor, acquire one of the following:</li>
+        <ol>
+          <li>Certified naturalization record and certified petition to naturalize</li>
+          <li>Certified letters of non-existence of a naturalization record (USCIS, National Archives, any county in which the ancestor resided) and certified Census record</li>
+        </ol>
+      </ol>
+      <h3>Translate your documents</h3>
+      <ol>
+        <li>Must be impeccably translated, probably worth getting a professional translator but could poss DIY if you’re really good</li>
+      </ol>
+      <h3>Show up to your appointment</h3>
+      <ol>
+        <li>Bring all your translated, apostilled documents and the required paperwork and payments</li>
+        <li>Good luck!</li>
+      </ol>
+    </div>
+  )
+}
+
+export default Guide;
