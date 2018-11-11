e0388ca7337e497f8b1e3a4409e2310701d948b7
pscroce <pscroce@gmail.com>
2018-11-10 23:29:34 -0500

    feat: add SectionNarrow to wrap other components for nice reading view

---
+++ src/components/SectionNarrow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function SectionNarrow(props) {
+  return (
+    <div className="section-narrow__wrapper">
+      {props.children}
+    </div>
+  )
+}
+
+export default SectionNarrow;
