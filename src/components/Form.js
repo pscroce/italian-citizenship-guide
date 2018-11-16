20df9f770e2c594db95ad72ca260fc09f2ece308
pscroce <pscroce@gmail.com>
2018-11-16 17:41:37 -0500

    feat: add FormV2 with more conversational experience and add related improvements to first Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -81,7 +81,8 @@ class Form extends Component {
       }),
       menuList: (provided, state) =>  ({
         ...provided,
-        backgroundColor: '#f7f7f7'
+        backgroundColor: '#f7f7f7',
+        padding: 0
       }),
       option: (provided, state) => ({
         ...provided,
@@ -91,7 +92,7 @@ class Form extends Component {
     return (
       <div className="form-wrapper">
         <h1>How To Get Italian Citizenship</h1>
-        <p>Most people with Italian ancestors can get Italian dual citizenship and an Italian passport, along with all the benefits of European citizenship such as the rights to live, work, establish business, and study in any of the 31 countries in the European Economic Area. The process is achievable but marred by misinformation and bureaucratic bloat.  </p>
+        <p>Most people with Italian ancestors can get Italian dual citizenship and an Italian passport. With it come all the benefits of European citizenship such as the rights to live, work, establish business, and study in any of the 31 countries in the European Economic Area. The process is achievable but marred by misinformation and bureaucratic bloat.  </p>
         <p>After I received Italian dual citizenship and an Italian passport in 2018 (a process I began in 2014 and which required thousands of hours of research, paperwork, phonecalls, etc.), I decided to make an Italian citizenship guide — something I hope will be actually useful, something better than the hundreds of outdated blogs and websites I had to wade through.</p>
         <p>Answer the questions below to check your eligibility.</p>
         <form onSubmit={this.handleSubmit}>
@@ -106,7 +107,46 @@ class Form extends Component {
               styles={selectStyles}
             />
 
-            { // If one's Italian ancestor is their parent, they don't need any additional ancestry but they do need to prove their parent did not naturalize or renounce citizenship before they were born and that they were born when their parents were "in wedlock."
+            { // If one's Italian ancestor is their grandparent, they also need documents for their connecting parent.
+              (this.state.ancestor.value === 'grandmother' || this.state.ancestor.value === 'grandfather') &&
+              <div>
+              <h3 className="no-margin-bottom">Now select your Italian parent</h3>
+                <p className="description">This is the daughter or son of your Italian grandparent.</p>
+                <Select
+                  value={parent}
+                  onChange={this.handleParentChange}
+                  options={parentOptions}
+                  theme={selectTheme}
+                  styles={selectStyles}
+                />
+              </div>
+            }
+
+            { // If one's Italian ancestor is their great-grandparent, they also need documents for their connecting grandparent and parent.
+              (this.state.ancestor.value === 'great-grandmother' || this.state.ancestor.value === 'great-grandfather') &&
+              <div>
+                <h3 className="no-margin-bottom">Now select your Italian grandparent</h3>
+                <p className="description">This is the daughter or son of your Italian great-grandparent.</p>
+                  <Select
+                    value={grandparent}
+                    onChange={this.handleGrandparentChange}
+                    options={grandparentOptions}
+                    theme={selectTheme}
+                    styles={selectStyles}
+                  />
+                <h3 className="no-margin-bottom">And also your Italian parent</h3>
+                <p className="description">This is the daughter or son of your grandparent who is the son or daughter of your Italian great-grandparent.</p>
+                  <Select
+                    value={parent}
+                    onChange={this.handleParentChange}
+                    options={parentOptions}
+                    theme={selectTheme}
+                    styles={selectStyles}
+                  />
+              </div>
+            }
+
+            { // They need to prove their parent did not naturalize or renounce citizenship before they were born and that they were born when their parents were "in wedlock."
               (this.state.ancestor.value) &&
               <div>
                 <h3 className="no-margin-bottom">Did your {this.state.ancestor.value} naturalize or renounce {this.state.ancestor.value.indexOf('mother') > -1 ? 'her' : 'his'} Italian citizenship for any reason before you were born?</h3>
@@ -127,53 +167,35 @@ class Form extends Component {
                   theme={selectTheme}
                   styles={selectStyles}
                 />
+                {
+                  (this.state.ancestor.value.indexOf('mother') > -1) &&
+                  <div>
+                    {
+                      (this.state.ancestor.value === 'mother') &&
+                      <h3 className="no-margin-bottom">Were you born after January 1, 1948?</h3>
+                    }
+                    {
+                      (this.state.ancestor.value === 'grandmother') &&
+                      <h3 className="no-margin-bottom">Was your {this.state.ancestor.value} born after January 1, 1948?</h3>
+                    }
+                    {
+                      (this.state.ancestor.value === 'great-grandmother') &&
+                      <h3 className="no-margin-bottom">Was your {this.state.ancestor.value} born after January 1, 1948?</h3>
+                    }
+                    <p className="description">The "1948 Rule" precludes women from passing Italian citizenship to children born before the date Italy became a Republic, January 1, 1948. </p>
+                    <Select
+                      value={ancestorNaturalized}
+                      onChange={this.handleNaturalizedChange}
+                      options={booleanOptions}
+                      theme={selectTheme}
+                      styles={selectStyles}
+                    />
+                  </div>
+                }
+                <button type="submit" id="buttonSubmitForm" className="-large">
+                  Check Eligibility
+                </button>
               </div>
-            }
-
-            { // If one's Italian ancestor is their grandparent, they also need documents for their connecting parent.
-              (this.state.ancestor.value === 'grandmother' || this.state.ancestor.value === 'grandfather') &&
-              <div>
-              <h3 className="no-margin-bottom">Now select your connecting parent</h3>
-                <p className="description">This is the daughter or son of your Italian grandparent.</p>
-                <Select
-                  value={parent}
-                  onChange={this.handleParentChange}
-                  options={parentOptions}
-                  theme={selectTheme}
-                  styles={selectStyles}
-                />
-              </div>
-            }
-
-            { // If one's Italian ancestor is their great-grandparent, they also need documents for their connecting grandparent and parent.
-              (this.state.ancestor.value === 'great-grandmother' || this.state.ancestor.value === 'great-grandfather') &&
-              <div>
-                <h3 className="no-margin-bottom">Now select your connecting grandparent</h3>
-                <p className="description">This is the daughter or son of your Italian great-grandparent.</p>
-                  <Select
-                    value={grandparent}
-                    onChange={this.handleGrandparentChange}
-                    options={grandparentOptions}
-                    theme={selectTheme}
-                    styles={selectStyles}
-                  />
-                <h3 className="no-margin-bottom">And also your connecting parent</h3>
-                <p className="description">This is the daughter or son of your grandparent who is the son or daughter of your Italian great-grandparent.</p>
-                  <Select
-                    value={parent}
-                    onChange={this.handleParentChange}
-                    options={parentOptions}
-                    theme={selectTheme}
-                    styles={selectStyles}
-                  />
-              </div>
-            }
-
-            {
-              (this.state.ancestor.value) &&
-              <button type="submit" id="buttonSubmitForm" className="input-and-button button">
-                Check Eligibility
-              </button>
             }
 
         </form>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header() {
   return (
     <header className="App-header">
       <h3>Italian Citizenship Guide</h3>
+      <a href="https://github.com/pscroce/italian-citizenship-guide" className="button -small" target="_blank" rel="noopener noreferrer">GitHub</a>
     </header>
   )
 }
