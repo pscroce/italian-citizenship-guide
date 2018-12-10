408c0a9b7ec269f6d67e0459d66db059a4a2ae4a
pscroce <pscroce@gmail.com>
2018-12-09 21:26:18 -0500

    feat: add basic answers to existing questions

--- src/components/pages/Faq.js
+++ src/components/pages/Faq.js
@@ -18,27 +18,27 @@ function Faq() {
 
           <div className="question__wrapper">
             <p className="question-title">Would I have to pay taxes in Italy?</p>
-            <p className="answer"></p>
+            <p className="answer">As long as you are not earning income in Italy, you do not need to pay taxes in Italy.</p>
           </div>
 
           <div className="question__wrapper">
             <p className="question-title">Would I have to serve in the military in Italy?</p>
-            <p className="answer"></p>
+            <p className="answer">Italy no longer has mandatory military service.</p>
           </div>
 
           <div className="question__wrapper">
             <p className="question-title">How much does it cost to apply for citizenship?</p>
-            <p className="answer"></p>
+            <p className="answer">The application itself costs 300 Euros. Finding, translating, certifying, and getting apostilles for your required documents will almost certainly cost more than that but will vary depending on how many documents you will need. It will likely cost at least a couple thousand dollars.</p>
           </div>
 
           <div className="question__wrapper">
             <p className="question-title">How long does it take to apply?</p>
-            <p className="answer"></p>
+            <p className="answer">In short, several years. You must apply through the Italian consulate in your jurisdiction (see consulate website to find yours) consulates have varying wait times, usually between 18 and 24 months. Gathering your documents can take a few years depending on how many documents you will need and how easily you are able to attain them. In most cases, it makes sense to book an appointment first then start gathering docuemnts since the wait time for an appointment is so long.</p>
           </div>
 
           <div className="question__wrapper">
             <p className="question-title">Will I be able to vote in Italy?</p>
-            <p className="answer"></p>
+            <p className="answer">Yes! Once you receive Italian citizenship, you are a full citizen with all the rights of a citizen, including the right to vote.</p>
           </div>
 
           <div className="question__wrapper">
